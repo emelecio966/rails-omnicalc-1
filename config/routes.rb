1f40271e3e0e0999edb19bdeb941a763a1785ac6
emelecio966 <143520582+emelecio966@users.noreply.github.com>
2024-04-29 14:34:08 +0000

    square

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get("/square/new", { :controller => "calc", :action => "square_new" }) 
+
+  get("/square/root", { :controller => "calc", :action => "square_root" })
+
+  get("/new/payment", { :controller => "calc", :action => "new_payment" }) 
+
+  get("/new/random", { :controller => "calc", :action => "new_random" }) 
   
+  get("/payment/results", { :controller => "calc", :action => "payment_results" }) 
+
+  get("/square_root/results", { :controller => "calc", :action => "square_results" }) 
+
+  get("/square_new/results", { :controller => "calc", :action => "new_square" }) 
+
+  get("/", { :controller => "calc", :action => "home" }) 
 end
